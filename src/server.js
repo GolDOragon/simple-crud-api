146309d0cc615dfa3b025754a4518a9ef84da2bd
Alexey Palanevich <alexey.palanevich@satelliteinnovations.io>
2021-11-28 19:39:09 +0300

    feat: implement body parser

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ import { createServer } from 'node:http';
 import { getRouter } from './routers/getRouter.mjs';
 import { personRouter } from './routers/personRouter.mjs';
 import { unknownRouter } from './routers/unknownRouter.mjs';
+import { getBody } from './helpers/getBody.mjs';
 
 export const server = createServer((request, response) => {
   const route = getRouter(request.url);
 
   switch (route) {
     case 'person':
-      personRouter(request, response);
+      getBody(request, response, personRouter);
       break;
 
     default:
