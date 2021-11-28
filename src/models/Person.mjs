d2af9e6b63cdedc872016fcf5086ff4da20a0be3
Alexey Palanevich <alexey.palanevich@satelliteinnovations.io>
2021-11-28 20:25:13 +0300

    feat: add basic entity class

--- src/models/Person.mjs
+++ src/models/Person.mjs
@@ -1,11 +1,22 @@
-export class Person {
+import { v4 as uuid } from 'uuid';
+import { Entity } from './Entity.mjs';
+
+export class Person extends Entity {
   /**
    * Simple person
    * @param {string} name
    * @param {number} age
    * @param {string[]} hobbies
    */
-  constructor({ name, age, hobbies }) {
+  constructor(properties) {
+    super();
+
+    if (!Person.isValidArgs(properties)) {
+      throw new Error('Invalid properties');
+    }
+
+    const { name, age, hobbies } = properties;
+
     this.name = name;
     this.age = age;
     this.hobbies = hobbies;
@@ -19,5 +30,14 @@ export class Person {
       Array.isArray(hobbies) &&
       hobbies.every((hobby) => typeof hobby === 'string')
     );
+  }
+
+  static toResponse({ name, age, hobbies, id }) {
+    return {
+      id,
+      name,
+      age,
+      hobbies,
+    };
   }
 }
